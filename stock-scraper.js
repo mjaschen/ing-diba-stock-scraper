1b089d2c6e9ef962267094d6b2717adbed60a9eb
carsten.stender <carsten.stender@innogames.com>
2018-10-22 12:04:32 +0200

    added json export and watchlist selection
    updated casperJS version to get rid of page errors

--- stock-scraper.js
+++ stock-scraper.js
@@ -4,6 +4,7 @@ const system = require('system');
 const utils  = require('utils');
 
 var quotes      = [];
+var quotes_json = "";
 var quotes_csv  = "";
 var quotes_prom = "";
 var now         = Math.floor(Date.now() / 1000);
@@ -20,10 +21,13 @@ function check_settings(){
     var safe_defaults = {
         login: '',
         password: '',
+        watchlist: '',
         url: 'https://wertpapiere.ing-diba.de/DE/Showpage.aspx?pageID=71',
         output: 'csv',
         outfolder: 'data/',
-        prom_prefix: ''
+        prom_prefix: '',
+        timestamp: true,
+        verbose: false
         };
 
     for (var k in safe_defaults) {
@@ -34,16 +38,19 @@ function check_settings(){
 }
 
 function get_filename() {
-    var filename = settings.outfolder + "quotes-" + now.toString() + ".txt";
+    var timestamp = settings.timestamp ? "-" + now.toString() : "";
+    var filename = settings.outfolder + "quotes" + timestamp + "." + settings.output;
     if (settings.output == 'prom') {
         filename = settings.outfolder + settings.prom_prefix + "quotes.prom";
-        }
+    }
     return filename;
 }
 
 check_settings();
 
 var casper = require('casper').create({
+    verbose: settings.verbose,
+
     clientScripts: [
         "node_modules/jquery/dist/jquery.js"
     ],
@@ -95,6 +102,25 @@ casper.then(function () {
 casper.wait(2000);
 
 casper.then(function () {
+    this.evaluate(function (watchlist) {
+        var select = $("select.sh_watchlistsDropDown")[0];
+        var found = false;
+        Array.prototype.forEach.call(select.children, function(opt, i){
+            if (!found && opt.innerHTML.indexOf(watchlist) != -1) {
+                select.selectedIndex = i;
+                found = true;
+            }
+        });
+        // dispatch change event in case there is some kind of validation
+        var evt = document.createEvent("UIEvents"); // or "HTMLEvents"
+        evt.initUIEvent("change", true, true);
+        select.dispatchEvent(evt);
+    }, settings.watchlist);
+});
+
+casper.wait(2000);
+
+casper.then(function () {
     quotes = this.evaluate(function () {
         var quotes = [];
         $("tr", "table.watchlist").each(function(idx, elem) {
@@ -103,13 +129,18 @@ casper.then(function () {
             if (! $tr.hasClass("first")) {
                 return
             }
+
+            changes=$("td.right span.arrow", $tr).text().replace(/%/g, "").split(/\s/);
+
             quotes.push({
                 id:             $("td.right span.tooltip", $tr).attr("itemid"),
                 isin:           $("td.isin", $tr_next).text(),
                 name:           $("td.name span.tooltip", $tr).text(),
-                count:          $tr.children("td").eq(1).children("span").first().text(),
-                quote_watch:    $("td.watchPrice span.tooltip", $tr).text(),
-                quote_current:  $("td.currentPrice span.tooltip", $tr).text(),
+                count:          $tr.children("td").eq(1).children("span").first().text().replace(' St.', ""),
+                quote_watch:    $("td.watchPrice span.tooltip", $tr).text().replace(' EUR', ""),
+                quote_current:  $("td.currentPrice span.tooltip", $tr).text().replace(' EUR', ""),
+                change_total:  	changes[1] || changes[0],
+                change_day:  	changes[1] ? changes[0] : "0"
             });
          })
 
@@ -118,6 +149,7 @@ casper.then(function () {
 });
 
 casper.then(function () {
+    quotes_json = '{"items":[';
     quotes_csv += "timestamp;id;isin;name;count;quote_watch;quote_current\n";
     quotes_prom = "#HELP " + settings.prom_prefix + "stock_price The stock price, either watched or current\n"
                 + "#HELP " + settings.prom_prefix + "stock_count The number of stocks in the watchlist\n"
@@ -130,48 +162,66 @@ casper.then(function () {
 
         // Prometheus Metric for the current price
         quotes_prom += settings.prom_prefix + "stock_price{"
-            + 'type="current",'
-            + 'id="'       + quotes[idx].id   + '",'
-            + 'isin="'     + quotes[idx].isin + '",'
-            + 'name="'     + quotes[idx].name + '",'
-            + 'currency="' + currency_current
-            + '"} ' + quotes[idx].quote_current.replace(/[^\d,-]/g, '').replace(/[,]/g, '.')
-            + "\n";
+        + 'type="current",'
+        + 'id="'       + quotes[idx].id   + '",'
+        + 'isin="'     + quotes[idx].isin + '",'
+        + 'name="'     + quotes[idx].name + '",'
+        + 'currency="' + currency_current
+        + '"} ' + quotes[idx].quote_current.replace(/[^\d,-]/g, '').replace(/[,]/g, '.')
+        + "\n";
 
         // Prometheus Metric for the watched price
         quotes_prom += settings.prom_prefix + "stock_price{"
-            + 'type="watch",'
-            + 'id="'       + quotes[idx].id   + '",'
-            + 'isin="'     + quotes[idx].isin + '",'
-            + 'name="'     + quotes[idx].name + '",'
-            + 'currency="' + currency_watch
-            + '"} ' + quotes[idx].quote_watch.replace(/[^\d,-]/g, '').replace(/[,]/g, '.')
-            + "\n";
+        + 'type="watch",'
+        + 'id="'       + quotes[idx].id   + '",'
+        + 'isin="'     + quotes[idx].isin + '",'
+        + 'name="'     + quotes[idx].name + '",'
+        + 'currency="' + currency_watch
+        + '"} ' + quotes[idx].quote_watch
+        + "\n";
 
         // Prometheus Metric for the watched stock count
         quotes_prom += settings.prom_prefix + "stock_count{"
-            + 'id="' + quotes[idx].id + '",'
-            + 'isin="' + quotes[idx].isin + '",'
-            + 'name="' + quotes[idx].name
-            + '"} ' + quotes[idx].count.replace(/[^\d,-]/g, '').replace(/[,]/g, '.')
-            + "\n";
+        + 'id="' + quotes[idx].id + '",'
+        + 'isin="' + quotes[idx].isin + '",'
+        + 'name="' + quotes[idx].name
+        + '"} ' + quotes[idx].count
+        + "\n";
 
         // Create the CSV line
         quotes_csv += now + ";"
-            + quotes[idx].id + ";"
-            + quotes[idx].isin + ";"
-            + quotes[idx].name + ";"
-            + quotes[idx].count + ";"
-            + quotes[idx].quote_watch + ";"
-            + quotes[idx].quote_current
-            + "\n";
+        + quotes[idx].id + ";"
+        + quotes[idx].isin + ";"
+        + quotes[idx].name + ";"
+        + quotes[idx].count + ";"
+        + quotes[idx].quote_watch + ";"
+        + quotes[idx].quote_current
+        + "\n";
 
+        //Creating JSON
+        if(idx != 0) {
+            quotes_json+=',';
+        }
+
+        quotes_json += '{' 
+            + '"isin":"' + quotes[idx].isin + '",'
+            + '"name":"' + quotes[idx].name + '",'
+            + '"count":' + quotes[idx].count.replace(',', '.') + ','
+            + '"quote_watch":' + quotes[idx].quote_watch.replace(',', '.') + ','
+            + '"quote_current":' + quotes[idx].quote_current.replace(',', '.') + ','
+            + '"change_day":' + quotes[idx].change_day.replace(',', '.') + ','
+            + '"change_total":' + quotes[idx].change_total.replace(',', '.');
+
+        quotes_json += '}';
     }
+    quotes_json += "]}";
 
     if (settings.output == 'prom') {
         fs.write(get_filename(), quotes_prom, 'w');
-    } else {
+    } else if (settings.output == 'csv'){
         fs.write(get_filename(), quotes_csv, 'w');
+    } else {
+        fs.write(get_filename(), quotes_json, 'w');
     }
 });
 
